eee0a691e4421820739032d0b9183e9f1549ef7c
Jed Long <jed.long@st-andrews.ac.uk>
2018-08-01 22:04:07 +0100

    esttheta

--- R/esttheta.R
+++ R/esttheta.R
@@ -155,7 +155,7 @@ esttheta <- function(traj,r,lower=0,upper=1,rand=NA,niter=10,tolerance=0.01,dmin
   
   if (plot){
     ord <- order(theta.val)
-    plot(theta.val[ord],LL.val[ord],xlab='theta',ylab='log-likelihood',type=n)
+    plot(theta.val[ord],LL.val[ord],xlab='theta',ylab='log-likelihood',type='n')
     ss <- smooth.spline(theta.val[ord],LL.val[ord],df=4)
     lines(ss)
     #points(theta.val[ord],-LL.val[ord],type='l')
