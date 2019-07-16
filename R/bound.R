0e770860a32d5e7aec0abd68abf577b4cc841573
Jed Long <jed.a.long@gmail.com>
2019-07-16 14:07:23 -0400

    boundary data

---
+++ R/bound.R
@@ -0,0 +1,17 @@
+#' Coarse boundary for Caribou example
+#'
+#' A coarse boundary used in the Caribou example of the rspUD method.
+#' 
+#' A linear boundary (hand drawn by the package author) to represent a coarse movement boundary for caribou movement to demonstrate the kernel and biased random bridge boundary corrections method. It is projected using the NAD83 UTM Zone 8N projection.
+#'
+# @references
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A \code{SpatialLines} feature.
+#' @name bound
+#' @examples
+#' data(bound)
+#' plot(bound)
+#-----------------------------------
+NULL
